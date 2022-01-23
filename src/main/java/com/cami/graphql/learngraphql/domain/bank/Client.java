2a0223dbf3fff946801a56dd8ef2e7b6c800f5da
cellariu <ratiu.camelia.delia@gmail.com>
2022-01-23 11:36:14 +0100

    added description in README.MD file

--- src/main/java/com/cami/graphql/learngraphql/domain/bank/Client.java
+++ src/main/java/com/cami/graphql/learngraphql/domain/bank/Client.java
@@ -1,7 +1,9 @@
 package com.cami.graphql.learngraphql.domain.bank;
 
+import jdk.jshell.Snippet;
 import lombok.Builder;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 import java.util.List;
 import java.util.UUID;
